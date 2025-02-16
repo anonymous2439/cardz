851199b6351d31cd07f8cb7bd31f4165562024bd
anonymous <mranonymous2439@gmail.com>
2025-02-16 15:58:01 +0800

    modify nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
   css: ["@/assets/css/global.scss"],
-})
+  
+  vite: {
+    server: {
+      host: true, // Expose to external devices
+      port: 8080, // Adjust if needed
+      hmr: {
+        clientPort: 8080, // Ensures the browser connects to the correct WebSocket port
+        path: '/_nuxt/'   // Match Nuxt's WebSocket path
+      }
+    }
+  }
+});
